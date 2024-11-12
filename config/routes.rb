30a8e5fefe3edbca33756de370a91ff8ea10ff9e
Kareem Mohllal <kareem.mohllal@gmail.com>
2024-11-12 14:56:28 +0200

    Remove the about action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'main#index'
-  match 'about', to: 'main#about', via: :get
 
   # get 'categories/index'
   # get 'categories/show'
